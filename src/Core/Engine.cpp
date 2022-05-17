d907341c3914f0e7b1c92346cb85a010e53ab3f4
Pierce Wang <piercewang@Pierces-MacBook-Pro.local>
2022-05-17 18:02:55 -0400

    TextureManager and window

--- src/Core/Engine.cpp
+++ src/Core/Engine.cpp
@@ -1,4 +1,5 @@
-#include "Engine.hpp"
+#include "Engine.h"
+#include "TextureManager.h"
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
@@ -16,29 +17,37 @@ bool Engine::Init()
         return false;
     }
 
-    std::cout << "test" << std::endl;
-
     m_Window = SDL_CreateWindow("TourmaWorld v0.1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                                 1280, 720, SDL_WINDOW_SHOWN);
-    if (m_Window == nullptr) {
+    if (m_Window == 0) {
         SDL_Log("Failed to create Window: %s", SDL_GetError());
         return false;
     }
 
-    m_Renderer = SDL_CreateRenderer(m_Window, -1, SDL_RENDERER_ACCELERATED); //| SDL_RENDERER_PRESENTVSYNC);
-    if (m_Window == nullptr) {
+    m_Renderer = SDL_CreateRenderer(m_Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC); //| SDL_RENDERER_PRESENTVSYNC);
+    if (m_Renderer == 0) {
         SDL_Log("Failed to create Renderer: %s", SDL_GetError());
         return false;
     }
 
-    return m_IsRunning = true;
+    TextureManager::GetInstance()->Load("tree", "../assets/textures/grass.png");
+
+    return (m_IsRunning = true);
 }
 bool Engine::Clean()
 {
+    TextureManager::GetInstance()->Clean();
+    SDL_DestroyRenderer(m_Renderer);
+    SDL_DestroyWindow(m_Window);
+    IMG_Quit();
+    SDL_Quit();
+
     return true;
 }
 void Engine::Quit()
-{}
+{
+    m_IsRunning = false;
+}
 
 void Engine::Update()
 {
@@ -47,7 +56,21 @@ void Engine::Update()
 void Engine::Render()
 {
     SDL_SetRenderDrawColor(m_Renderer, 124, 218, 254, 255);
+    SDL_RenderClear(m_Renderer);
+
+    TextureManager::GetInstance()->Draw("tree", 100, 100, 347, 465);
     SDL_RenderPresent(m_Renderer);
 }
 void Engine::Events()
-{}
+{
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch(event.type) {
+        case SDL_QUIT:
+            Quit();
+            break;
+        default:
+            break;
+    }
+
+}
